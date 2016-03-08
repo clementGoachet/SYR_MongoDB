643783c94d25ac5ce43418f8cc1cf646ffe79a4c
ClementG <Clement.goachet@gmail.com>
2016-03-08 12:27:39 +0100

    Init

---
+++ tp7/src/main/java/fr/istic/syr/tp7/App.java
@@ -0,0 +1,13 @@
+package fr.istic.syr.tp7;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
